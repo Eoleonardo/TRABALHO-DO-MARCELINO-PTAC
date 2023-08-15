eabfe2322d3ce02b03c8cb35a49d50bf310a384c
Eoleonardo <leonardobeneval@gmail.com>
2023-08-15 15:49:14 -0400

    Update handlerAcessAPI.js

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,7 +1,30 @@
 'use server'
+const users = [
+{
+    name:'usuario1',
+    email:'usuario1@gmail.com',
+    password:'123',
+    token:'yJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+},
+{
+    name:'usuario2',
+    email:'usuario2@gmail.com',
+    password:'321',
+    token:'yJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+},
+{
+    name:'usuario3',
+    email:'usuario3@gmail.com',
+    password:'231',
+    token:'yJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+}
+]
 
-const getUserAuthenticated = (user) => {
-
+const getUserAuthenticated = (users, user) => {
+for (let i = 0; i < users.length; i++){
+    if (users[i].email === user.email && users[i].password === user.password)
+    return true;
+}
 }
 
 const getUsers = () =>{
