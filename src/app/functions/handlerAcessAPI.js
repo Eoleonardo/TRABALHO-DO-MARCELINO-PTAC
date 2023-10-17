95de9839ca3cf630f3e52a37bc904766e46aae59
httpgabimarquez <leonardobeneval@gmail.com>
2023-10-17 16:00:40 -0400

    Update handlerAcessAPI.js

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -3,7 +3,15 @@
 const url ="https://aula-17-10-xi.vercel.app"
 
 const getUserAuthenticated = async (user) => {
-    const responseOfApi = await fetch(url + "/user/authenticate");
+    const responseOfApi = await fetch(url + "/user/authenticate", 
+    {
+      method:"POST",
+      headers: {"Contente-Type":"application/json"}, 
+      body: JSON.stringify(user)
+    }
+    );
+    const userAuth =await responseOfApi.json();
+    return userAuth;
 }
   const getUsers = async () => {
    
