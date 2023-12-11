4cd1d8d4d6b361e873623770bdf2b6c5892a3aa2
Eoleonardo <leonardobeneval@gmail.com>
2023-12-11 16:43:41 -0400

    ;

--- src/app/pages/alterar/[id]/page.js
+++ src/app/pages/alterar/[id]/page.js
@@ -4,7 +4,6 @@ import React, {useState} from "react";
 import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import Navbar from "@/app/componentes/navbar";
-import '../alterar.css';
 import { updateUser } from "@/app/functions/handlerAcessAPI";
 import {useRouter} from 'next/navigation';
 
@@ -14,7 +13,7 @@ export default function Alterar({params}) {
   const [user, setUser] = useState({
 name:'',
 email:'',
-password:''
+password:'',
   });
 
   const { push } = useRouter();
@@ -40,11 +39,11 @@ password:''
       <form onSubmit={alterar}>
         <div className="caixa">
           <p>NOME</p>
-          <input type="text" required />
+          <input type="text" required  onChange={(e) => { setUser({ ...user, name: e.target.value })}} />
           <p>EMAIL</p>
-          <input type="email" required />
+          <input type="email" required  onChange={(e) => { setUser({ ...user, email: e.target.value })}} />
           <p>SENHA</p>
-          <input type="password" required />
+          <input type="password" required  onChange={(e) => { setUser({ ...user, password: e.target.value })}} />
           <p></p>
           <button className="btn">Alterar</button>
         </div>
